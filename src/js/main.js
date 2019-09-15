ad67b7e193215c43078e6850e8061d29847552a5
Jakub JJ <jakubjonakowski@gmail.com>
2019-09-15 23:11:08 +0200

    Additional feature

--- src/js/main.js
+++ src/js/main.js
@@ -18,20 +18,28 @@ if ('serviceWorker' in navigator) {
 
 // Tablice //
 
-const addGlass = document.querySelector('.drink__buttons--add--js');
-const remGlass = document.querySelector('.drink__buttons--del--js');
-const counterr = document.querySelector('.drink__glass--no--js');
+const addWater = document.querySelector('.water__buttonadd--js');
+const remWater = document.querySelector('.water__buttondel--js');
+
+const addBeer = document.querySelector('.beer__buttonadd--js');
+const remBeer = document.querySelector('.beer__buttondel--js');
+
+const addWodka = document.querySelector('.wodka__buttonadd--js');
+const remWodka = document.querySelector('.wodka__buttondel--js');
+
+const remAll = document.querySelector('.del__all--js');
+
+const water = document.querySelector('.water__no--js');
+const beer = document.querySelector('.beer__no--js');
+const wodka = document.querySelector('.wodka__no--js');
+
 const date = new Date().toISOString().slice(0, 10);
 let tablicaDaty = [date];
+let tablicaDrink = [water,beer,wodka];
 
-if(!localStorage.getItem(`day ${date}`)) {
-  counterr.innerHTML = "0";
-  }
-  else {
-    counterr.innerHTML = localStorage.getItem(`day ${date}`);
-  }
+// FUNKCJE //
 
-addGlass.addEventListener('click', (e) => {
+function sprawdzTablice(x,y) {
   if(!localStorage.getItem("tablicaDaty", date)) {
     localStorage.setItem("tablicaDaty", JSON.stringify(tablicaDaty)); }
     else {
@@ -43,16 +51,80 @@ addGlass.addEventListener('click', (e) => {
     }
   
   if(!localStorage.getItem(`day ${date}`)) {
-    localStorage.setItem(`day ${date}`, 0);
+    let tablicaDrink = [0,0,0];
+    localStorage.setItem(`day ${date}`, JSON.stringify(tablicaDrink));
     }
-  
-  localStorage.setItem(`day ${date}`, parseInt(localStorage.getItem(`day ${date}`)) +1);
-  ++counterr.innerHTML;
+
+    tablicaDrink = JSON.parse(localStorage.getItem(`day ${date}`, tablicaDrink));
+    ++tablicaDrink[x];
+    localStorage.setItem(`day ${date}`, JSON.stringify(tablicaDrink));
+    ++y.innerHTML;
+}
+
+function zmniejszLicznik(x,y) {
+  if(localStorage.getItem(`day ${date}`)) {
+    tablicaDrink = JSON.parse(localStorage.getItem(`day ${date}`, tablicaDrink));
+
+    if(tablicaDrink[x] > 0) {  
+    --tablicaDrink[x];
+    localStorage.setItem(`day ${date}`, JSON.stringify(tablicaDrink));
+    --y.innerHTML;
+    }
+  }
+}
+
+// START //
+
+if(!localStorage.getItem(`day ${date}`)) {
+  water.innerHTML = 0;
+  beer.innerHTML = 0;
+  wodka.innerHTML = 0;
+  }
+  else {
+    tablicaDrink = JSON.parse(localStorage.getItem(`day ${date}`, tablicaDrink));
+    water.innerHTML = tablicaDrink[0];
+    beer.innerHTML = tablicaDrink[1];
+    wodka.innerHTML = tablicaDrink[2];
+  }
+
+// WODA //
+
+addWater.addEventListener('click', (e) => {
+  sprawdzTablice(0,water);
 })
 
-remGlass.addEventListener('click', (e) => {
-  if(localStorage.getItem(`day ${date}`) > 0) {  
-  localStorage.setItem(`day ${date}`, parseInt(localStorage.getItem(`day ${date}`)) -1);
-  --counterr.innerHTML;
+remWater.addEventListener('click', (e) => {
+  zmniejszLicznik(0,water);
+})
+
+// PIWO //
+
+addBeer.addEventListener('click', (e) => {
+  sprawdzTablice(1,beer);
+})
+
+remBeer.addEventListener('click', (e) => {
+  zmniejszLicznik(1,beer);
+})
+
+// WODKA //
+
+addWodka.addEventListener('click', (e) => {
+  sprawdzTablice(2,wodka);
+})
+
+remWodka.addEventListener('click', (e) => {
+  zmniejszLicznik(2,wodka);
+})
+
+// USUN WSZYSTKO //
+
+remAll.addEventListener('click', (e) => {
+  if(localStorage.getItem(`day ${date}`)) {
+  let tablicaDrink = [0,0,0];
+  localStorage.setItem(`day ${date}`, JSON.stringify(tablicaDrink));
+  water.innerHTML = 0;
+  beer.innerHTML = 0;
+  wodka.innerHTML = 0;
   }
 })
