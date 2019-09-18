17e874c2891c30438949d3ccaaf935cfc8688187
Jakub JJ <jakubjonakowski@gmail.com>
2019-09-18 15:46:48 +0200

    calendar

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG, WSASYSNOTREADY } from "constants";
 
 // service worker registration - remove if you're not going to use it
 
@@ -16,30 +16,46 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+// Zmienne //
+
+let addWater, remWater, addBeer, remBeer, addWodka, remWodka, remAll, water, beer, wodka, date, tablicaDaty, tablicaDrink, calendar, calButton, naglowek;
+
+
 // Tablice //
 
-const addWater = document.querySelector('.water__buttonadd--js');
-const remWater = document.querySelector('.water__buttondel--js');
+addWater = document.querySelector('.water__buttonadd--js');
+remWater = document.querySelector('.water__buttondel--js');
 
-const addBeer = document.querySelector('.beer__buttonadd--js');
-const remBeer = document.querySelector('.beer__buttondel--js');
+addBeer = document.querySelector('.beer__buttonadd--js');
+remBeer = document.querySelector('.beer__buttondel--js');
 
-const addWodka = document.querySelector('.wodka__buttonadd--js');
-const remWodka = document.querySelector('.wodka__buttondel--js');
+addWodka = document.querySelector('.wodka__buttonadd--js');
+remWodka = document.querySelector('.wodka__buttondel--js');
 
-const remAll = document.querySelector('.del__all--js');
+remAll = document.querySelector('.del__all--js');
 
-const water = document.querySelector('.water__no--js');
-const beer = document.querySelector('.beer__no--js');
-const wodka = document.querySelector('.wodka__no--js');
+water = document.querySelector('.water__no--js');
+beer = document.querySelector('.beer__no--js');
+wodka = document.querySelector('.wodka__no--js');
 
-const date = new Date().toISOString().slice(0, 10);
-let tablicaDaty = [date];
-let tablicaDrink = [water,beer,wodka];
+date = new Date().toISOString().slice(0, 10);
+tablicaDaty = [date];
+tablicaDrink = [water,beer,wodka];
+
+// KALENDARZ //
+calendar = document.querySelector('.datepicker--js');
+calButton = document.querySelector('.calendar__button--js');
+naglowek = document.querySelector('.inputText--js');
+
+let calVal;
+
+calButton.addEventListener('click', (e) => {
+  kalendarz();
+})
 
 // FUNKCJE //
 
-function sprawdzTablice(x,y) {
+function sprawdzTablice(pozTab,nazwaPlynu) {
   if(!localStorage.getItem("tablicaDaty", date)) {
     localStorage.setItem("tablicaDaty", JSON.stringify(tablicaDaty)); }
     else {
@@ -54,24 +70,49 @@ function sprawdzTablice(x,y) {
     let tablicaDrink = [0,0,0];
     localStorage.setItem(`day ${date}`, JSON.stringify(tablicaDrink));
     }
-
+    
     tablicaDrink = JSON.parse(localStorage.getItem(`day ${date}`, tablicaDrink));
-    ++tablicaDrink[x];
+    ++tablicaDrink[pozTab];
     localStorage.setItem(`day ${date}`, JSON.stringify(tablicaDrink));
-    ++y.innerHTML;
+    ++nazwaPlynu.innerHTML;
 }
 
-function zmniejszLicznik(x,y) {
+function zmniejszLicznik(pozTab,nazwaPlynu) {
   if(localStorage.getItem(`day ${date}`)) {
     tablicaDrink = JSON.parse(localStorage.getItem(`day ${date}`, tablicaDrink));
 
-    if(tablicaDrink[x] > 0) {  
-    --tablicaDrink[x];
+    if(tablicaDrink[pozTab] > 0) {  
+    --tablicaDrink[pozTab];
     localStorage.setItem(`day ${date}`, JSON.stringify(tablicaDrink));
-    --y.innerHTML;
+    --nazwaPlynu.innerHTML;
     }
   }
 }
+
+function kalendarz () {
+  let calVal = calendar.value;
+  calVal = calVal.slice(-4) + "-" + calVal.slice(0,2) + "-" + calVal.slice(3, 5);
+
+  if (calVal) {
+    if (localStorage.getItem(`day ${calVal}`)) {
+      let wysw = JSON.parse(localStorage.getItem(`day ${calVal}`));
+      naglowek.innerHTML = `Dnia ${calVal} wypiłeś ${wysw[0]} szklanek wody, ${wysw[1]} piwa i ${wysw[2]} wódki`;
+      tablicaDrink = JSON.parse(localStorage.getItem(`day ${calVal}`, tablicaDrink));
+      water.innerHTML = tablicaDrink[0];
+      beer.innerHTML = tablicaDrink[1];
+      wodka.innerHTML = tablicaDrink[2];
+    }
+    
+  }
+  else {
+    tablicaDrink = JSON.parse(localStorage.getItem(`day ${date}`, tablicaDrink));
+    water.innerHTML = tablicaDrink[0];
+    beer.innerHTML = tablicaDrink[1];
+    wodka.innerHTML = tablicaDrink[2];
+    naglowek.innerHTML=null;
+  }
+}
+
 
 // START //
 
